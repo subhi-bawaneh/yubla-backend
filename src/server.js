a2c9f496e1cde2efec554660ff355a4189c9ed81
Subhi Bawaneh <subhi@nesmapartners.com>
2026-03-11 03:33:29 +0300

    icons

--- src/server.js
+++ src/server.js
@@ -21,6 +21,7 @@ const defaultOrigins =
     : [
         'http://localhost:5173',
         'http://127.0.0.1:5173',
+        'http://192.168.0.100:5173/',
         'https://yubla-frontend.vercel.app',
         'https://www.qiyasjo.org'
       ];
